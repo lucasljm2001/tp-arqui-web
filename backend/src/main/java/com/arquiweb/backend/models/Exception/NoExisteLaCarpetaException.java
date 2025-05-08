ab9377d9ef5021ee8ce80a89ffcae7ae60a34ef6
lucas.martinez2 <lucas.martinez@naranjax.com>
2025-05-08 19:29:57 -0300

    Corrijo nombres de tests

--- backend/src/main/java/com/arquiweb/backend/models/Exception/NoExisteLaCarpetaException.java
+++ backend/src/main/java/com/arquiweb/backend/models/Exception/NoExisteLaCarpetaException.java
@@ -3,6 +3,6 @@ package com.arquiweb.backend.models.Exception;
 public class NoExisteLaCarpetaException  extends  RuntimeException {
     @Override
     public String getMessage() {
-        return "La Apuesta no existe";
+        return "La carpeta no existe";
     }
 }
